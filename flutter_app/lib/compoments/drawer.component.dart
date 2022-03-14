a7a8f404e9f6944d9051ecf68cb9879e8bf5edaf
chauchuty <chauchuty@alunos.utfpr.edu.br>
2022-03-14 20:24:47 -0300

    add: drawer

---
+++ flutter_app/lib/compoments/drawer.component.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class DrawerComponent extends StatelessWidget {
+  const DrawerComponent({Key? key}) : super(key: key);
+
+  // This widget is the root of your DrawerComponentlication.
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+            child: Text('Drawer Header'),
+          ),
+          ListTile(
+            title: const Text('Item 1'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+          ListTile(
+            title: const Text('Item 2'),
+            onTap: () {
+              // Update the state of the app.
+              // ...
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
